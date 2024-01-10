1f066d91a8fdcb8aec268c3f9ae4ba1d1b4f1f25
checkthistape <litos.art2006@gmail.com>
2024-01-10 13:11:57 +0100

    Carousel

---
+++ filmweb/Carousel/CarouselItem.cs
@@ -0,0 +1,9 @@
+﻿namespace filmweb.Carousel
+{
+    public class CarouselItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ filmweb/Carousel/CarouselViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filmweb.Carousel
+{
+    public class CarouselViewModel : ObservableObject
+    {
+        public ObservableCollection<CarouselItem> CarouselItems { get; set; } = new();
+
+        public CarouselViewModel()
+        {
+            CarouselItems.Add(new CarouselItem()
+            {
+                Title = "Title1",
+                Description = "Description1",
+                ImageURL = "https://www.syncfusion.com/blogs/wp-content/uploads/2023/01/Exploring-the-New-.NET-MAUI-Backdrop.png"
+            });
+
+            CarouselItems.Add(new CarouselItem()
+            {
+                Title = "Title2",
+                Description = "Description2",
+                ImageURL = "https://www.syncfusion.com/blogs/wp-content/uploads/2023/01/Exploring-the-New-.NET-MAUI-Backdrop.png"
+            });
+
+            CarouselItems.Add(new CarouselItem()
+            {
+                Title = "Title3",
+                Description = "Description3",
+                ImageURL = "https://www.syncfusion.com/blogs/wp-content/uploads/2023/01/Exploring-the-New-.NET-MAUI-Backdrop.png"
+            });
+
+        }
+    }
+}
