9e66612d4e067aee7f3c1dfd2a2f130abef856eb
checkthistape <litos.art2006@gmail.com>
2024-01-16 01:38:30 +0100

    Great deletition

--- filmweb/Carousel/CarouselItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace filmweb.Carousel
-{
-    public class CarouselItem
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string ImageURL { get; set; }
-    }
-}
